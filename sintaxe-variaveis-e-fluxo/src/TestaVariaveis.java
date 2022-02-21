d1f1d7a6face34f93b59c641f9090ae54f2ee3ec
brunomdrrosa <brunomdr46@gmail.com>
2022-02-21 15:00:16 -0300

    Praticando condicionais

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,18 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Ola novo teste");
+		
+		int idade;
+		idade = 19;
+		System.out.println(idade);
+		
+		idade = 10 + 9;
+		System.out.println(idade);
+		
+		idade = (2 * 5) + 9;
+		System.out.println(idade);
+		
+		System.out.println("A idade é " + idade);
+	}
+}
